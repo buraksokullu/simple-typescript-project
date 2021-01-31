b33f2b2a63eb5638d0b27d9b2401ae3e01523014
Burak Sokullu <burak.sokullu@hepsiburada.com>
2021-01-31 19:13:21 +0300

    First commit

---
+++ app.js
@@ -0,0 +1,7 @@
+function addNumber(n1, n2) {
+    return n1 + n2;
+}
+var number1 = 5;
+var number2 = 2.8;
+var result = addNumber(number1, number2);
+console.log(result);
---
+++ app.ts
@@ -0,0 +1,9 @@
+function addNumber(n1: number, n2: number) {
+  return n1 + n2;
+}
+
+const number1 = 5;
+const number2 = 2.8;
+
+const result = addNumber(number1, number2);
+console.log(result);
